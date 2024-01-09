3e37117376082d038d01bf47d6b6ae1bde89c3fb
Rishabh Rajora <91421022+coder-rajora@users.noreply.github.com>
2024-01-09 06:17:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = "33"  // inn sab ko uncomment karke dekh lena kya output aa raha hai.
+//let score = null
+//let score = undefined
+//let score = true
+// let score = "Rishabh"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Some notes:
+// "33" => 33
+// "33abc" => NaN (Not a Number)  //output to ye dega but iska type abhi bhi number hi hai.
+//true => 1 ; false => 0
+
+//********************************************************************************************************************************
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Some notes:
+// 1 => true ; 0 => false
+// "" => false
+// "Rishabh" => true
+
+//********************************************************************************************************************************
+
+let someNumber = 33
+let stringNumber = String(someNumber) 
+console.log(stringNumber)
+console.log(typeof stringNumber);  // output dikhne mai number hai but string mai convert ho chuka hai.
